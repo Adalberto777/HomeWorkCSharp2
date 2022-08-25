390e540229a1481a23a2dcf30541c06fd148d089
Adalberto777 <albert.tliashev@gmail.com>
2022-08-25 19:14:57 +0500

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Программа для вывода второй по счету цифры, трехзначного числа");
+
+int number = new Random().Next(100,1000);
+Console.WriteLine($"Случайное трехзначное число: {number}");
+
+Console.WriteLine($"вторая цифра: {number} это {(number % 100)/10}");
