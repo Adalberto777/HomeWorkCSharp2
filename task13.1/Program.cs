ae9ae94ea7937645317fd2bf4399b8b66d6c0cff
Adalberto777 <albert.tliashev@gmail.com>
2022-08-27 20:48:48 +0500

    task13.1

---
+++ task13.1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет");
+
+Console.WriteLine("Enter a positive integer");
+
+int number = int.Parse(Console.ReadLine());
+int b = number % 100;
+Console.WriteLine(b);
+if(number % 100 == 0 && number % 100 == number)
+Console.WriteLine("no fre number");
+
