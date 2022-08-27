fd8e423b6af2ff521bcae9820983ca380baf783b
Adalberto777 <albert.tliashev@gmail.com>
2022-08-27 19:52:18 +0500

    task15

---
+++ task15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("программа, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным");
+
+Console.WriteLine("Enter a day of week (number from 1 to 7)");
+
+int number = int.Parse(Console.ReadLine());
+
+if(number < 6)
+{
+    Console.WriteLine("рабочий день");
+}
+else
+{
+    Console.WriteLine("выходной день");
+}
