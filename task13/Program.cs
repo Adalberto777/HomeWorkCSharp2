3557218f8f9d93338b182250e1a37b2c389fc31a
Adalberto777 <albert.tliashev@gmail.com>
2022-08-27 15:01:25 +0500

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет");
+
+Console.WriteLine("Enter a positive integer");
+
+string input = Console.ReadLine();
+int lenght = lenght.input;
+
+Console.WriteLine(lenght);
